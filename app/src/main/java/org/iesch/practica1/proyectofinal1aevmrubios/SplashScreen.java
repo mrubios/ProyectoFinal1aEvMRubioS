de636edbec3a0ebc8483028a471e3952f2ce77d8
michel <mrubios@iesch.org>
2022-12-08 10:03:22 +0100

    Splash Screen + icono

---
+++ app/src/main/java/org/iesch/practica1/proyectofinal1aevmrubios/SplashScreen.java
@@ -0,0 +1,30 @@
+package org.iesch.practica1.proyectofinal1aevmrubios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class SplashScreen extends AppCompatActivity {
+
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
